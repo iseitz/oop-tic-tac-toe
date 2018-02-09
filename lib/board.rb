a517d8c3d6aafd7dee949bf8f5743c900e45b160
Irina Karchebnaya <irenika@gmail.com>
2018-02-09 12:09:46 -0500

    winner function defined but not yet tested

--- lib/board.rb
+++ lib/board.rb
@@ -50,4 +50,38 @@ class Board
 
     return false
   end
+
+  def winner?
+    @board.each do |row|
+      row.each do |space|
+        if !space.nil?
+          if row[0] == row[1] == row[2]
+            true
+            space
+          # elsif @board[1][0] == @board[1][1] == @board[1][2]
+          #   true
+          #   space
+          # elsif @board[2][0] == @board[2][1] == @board[2][2]
+          #   true
+          #   space
+          elsif @board[0][0] == @board[1][0] == @board[2][0]
+            true
+            space
+          elsif @board[0][1] == @board[1][1] == @board[2][1]
+            true
+            space
+          elsif @board[0][2] == @board[1][2] == @board[2][2]
+            true
+            space
+          elsif @board[0][0] == @board[1][1] == @board[2][2]
+            true
+            space
+          elsif @board[0][2] == @board[1][1] == @board[2][0]
+            true
+            space
+          end
+        end
+        space
+  end
+
 end
--- lib/main.rb
+++ lib/main.rb
@@ -2,10 +2,26 @@
 # - Board
 # - Spots
 # - Players
+require_relative'player'
+require_relative'board'
 
+board = Board.new
 x_player = Player.new('x')
 o_player = Player.new('o')
 
+players = [x_player, o_player].shuffle
+turn_index = 0
 while board.has_empty_spaces? && !board.winner?
+   puts "It is #{players[turn_index].character} turn"
 
+   print "What row do you want to play?"
+   row_index = gets.chomp.to_i
+
+   print "What column do you want tp play?"
+   col_index = gets.chomp.to_i
+
+   board.add_turn(players[turn_index].character, row_index, col_index)
+   puts board.print
+
+   turn_index = turn_index == 0 ? 1 : 0
 end
